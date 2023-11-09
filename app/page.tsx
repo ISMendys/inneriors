fe34bce5bac682fac26559f755499b8720fe8895
Isael Santos <isael@offertech.com.br>
2023-11-09 06:19:10 -0300

    INN-01 Added initial page select theme

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { ModeToggle } from "@/components/theme-select"
+export default function Home() {
+  return (
+    <>
+      <ModeToggle/>
+      <section>
+        teste
+      </section>
+    </>
+  )
+}
